1ed70cee4ae8c2398bef433929c54adf412b1459
dewabagas <dewabagasql@gmail.com>
2024-03-27 13:32:35 +0700

    implements go_router and create router_provider and modify some pages

--- lib/presentation/pages/login_page/login_page.dart
+++ lib/presentation/pages/login_page/login_page.dart
@@ -1,13 +1,6 @@
-import 'package:flix_id/data/dummies/dummy_authentication.dart';
-import 'package:flix_id/data/dummies/dummy_user_repository.dart';
-import 'package:flix_id/data/firebase/firebase_authentication.dart';
-import 'package:flix_id/data/firebase/firebase_user_repository.dart';
-import 'package:flix_id/domain/usecases/login/login.dart';
-import 'package:flix_id/presentation/pages/main_page/main_page.dart';
-import 'package:flix_id/presentation/providers/usecases/login_provider.dart';
-import 'package:flutter/foundation.dart';
+import 'package:flix_id/presentation/providers/router/router_provider.dart';
+import 'package:flix_id/presentation/providers/user_data/user_data_provider.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class LoginPage extends ConsumerWidget {
@@ -15,25 +8,21 @@ class LoginPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    ref.listen(userDataProvider, (previous, next) {
+      if (next is AsyncData) {
+        if(next.value != null) {
+          ref.read(routerProvider).goNamed('main');
+        }
+      } else if (next is AsyncError) {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(next.error.toString())));
+      }
+    });
     return Scaffold(
         appBar: AppBar(title: Text('Login')),
         body: Center(
           child: ElevatedButton(
               onPressed: () {
-                Login login = ref.watch(loginProvider);
-
-                login(LoginParams(
-                        email: 'bagas.dewa@upi.edu', password: 'bagas1'))
-                    .then((result) {
-                  if (result.isSuccess) {
-                    Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) =>
-                            MainPage(user: result.resultValue!)));
-                  } else {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(content: Text(result.errorMessage!)));
-                  }
-                });
+               ref.read(userDataProvider.notifier).login(email: 'bagas.dewa@upi.edu', password: '123456');
               },
               child: const Text('Login')),
         ));
